b12845a043132203ed0d662ec4b42a0e96f348e9
xconde <mxf942@my.utsa.edu>
2020-08-15 18:46:37 -0500

    Price checker command available for discord bot. Uses "last_trade_price" from raw tuple

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
 import os
-from loginRh import rh
+import re
+import time
+from rh import rh
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 client = commands.Bot(command_prefix='.')
 
@@ -10,11 +13,21 @@ client = commands.Bot(command_prefix='.')
 @client.event
 async def on_ready():
     print('Bot is ready')
-    rh.print_quote("ESTC")
+
+
+@client.command(name='p')
+async def priceCheck(ctx, arg1):
+    if re.match(r'\b[a-zA-Z]{1,4}\b', arg1):
+        temp = rh.get_quote_list(arg1.upper(), "symbol,last_trade_price")
+        data = temp[0]
+        await ctx.send(arg1.upper() + ": $" + str(round(float(data[1]), 2)))
 
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
+
+
+
 
 
 
--- rh.py
+++ rh.py
@@ -8,3 +8,11 @@ if rh.login(username=os.getenv('USER'), password=os.getenv('PASS')):
     print("Created Robinhood instance.")
 else:
     print("Failed to create Robinhood instance.")
+
+"""
+def print_quote(self, stock=""):  # pragma: no cover
+    data = self.get_quote_list(stock, "symbol,last_trade_price")
+    for item in data:
+        quote_str = item[0] + ": $" + item[1]
+        return quote_str
+"""
