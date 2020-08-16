aa776ad703e283fc67a64e287b889d7086d350b1
xconde <mxf942@my.utsa.edu>
2020-08-15 19:16:55 -0500

    Added percent to the price check, using previous close data

--- bot.py
+++ bot.py
@@ -19,8 +19,13 @@ async def on_ready():
 async def priceCheck(ctx, arg1):
     if re.match(r'\b[a-zA-Z]{1,4}\b', arg1):
         temp = rh.get_quote_list(arg1.upper(), "symbol,last_trade_price")
+        temp2 = rh.adjusted_previous_close(arg1.upper())
+        prev_close = temp2[0]
+        prev_close = round(float(prev_close[0]), 2)
         data = temp[0]
-        await ctx.send(arg1.upper() + ": $" + str(round(float(data[1]), 2)))
+        data = round(float(data[1]), 2)
+        percent = round(((data-prev_close)/prev_close*100), 2)
+        await ctx.send(arg1.upper() + ": $" + str(data) + "    " + str(percent) + "%")
 
 
 load_dotenv()
