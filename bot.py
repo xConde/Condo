2ac1bc7d197c9fc8aa7e146696c88ca07f5a0231
xconde <mxf942@my.utsa.edu>
2020-08-15 20:28:10 -0500

    create priceCheckList which can priceCheck several tickers at once

--- bot.py
+++ bot.py
@@ -15,24 +15,33 @@ async def on_ready():
     print('Bot is ready')
 
 
+def pc(arg1):
+    temp = rh.get_quote_list(arg1.upper(), "symbol,last_trade_price")
+    temp2 = rh.adjusted_previous_close(arg1.upper())
+    prev_close = temp2[0]
+    prev_close = round(float(prev_close[0]), 2)
+    data = temp[0]
+    data = round(float(data[1]), 2)
+    percent = round(((data - prev_close) / prev_close * 100), 2)
+    return arg1.upper() + ": $" + str(data) + "    " + str(percent) + "%"
+
+
 @client.command(name='p')
 async def priceCheck(ctx, arg1):
     if re.match(r'\b[a-zA-Z]{1,4}\b', arg1):
-        temp = rh.get_quote_list(arg1.upper(), "symbol,last_trade_price")
-        temp2 = rh.adjusted_previous_close(arg1.upper())
-        prev_close = temp2[0]
-        prev_close = round(float(prev_close[0]), 2)
-        data = temp[0]
-        data = round(float(data[1]), 2)
-        percent = round(((data-prev_close)/prev_close*100), 2)
-        await ctx.send(arg1.upper() + ": $" + str(data) + "    " + str(percent) + "%")
+        res = pc(arg1)
+        await ctx.send(res)
+
+
+@client.command(name='pp')
+async def priceCheckList(ctx, *args):
+    res=""
+    for arg in args:
+        if re.match(r'\b[a-zA-Z]{1,4}\b', arg):
+            pcList = pc(arg)
+            res += pcList + '\n'
+    await ctx.send(res)
 
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
-
-
-
-
-
-
