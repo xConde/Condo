dc317ec698e29e6411d51966a67521d30f50380b
xconde <mxf942@my.utsa.edu>
2020-12-08 08:48:57 -0600

    Refactor OptionCommands.py

--- bot/commands/OptionCommands.py
+++ bot/commands/OptionCommands.py
@@ -9,9 +9,11 @@ class OptionCommands(commands.Cog):
 
     @commands.command(name='option')
     async def findOptions(self, ctx, stock=None, strike=None, type=None, expir=None):
-        """Takes in a stock ticker, strike, an optional expiration date (defaulted to friday expiration [if applicable]),``
-         a type (defaulted to call) and prints the information (Strike, price, volume, OI) to discord.
+        """Takes in a stock ticker, strike, an optional type (defaulted to call), and an optional expiration date
+         (defaulted to friday expiration [if applicable]),``and prints the information (Strike, price, volume, OI)
+         to discord.
 
+        :param strike:
         :param ctx:
         :param stock: {1-5} character stock-ticker.
         :param type: Defaulted to 'call'. Can be either 'call' or 'put'.
@@ -19,17 +21,7 @@ class OptionCommands(commands.Cog):
 
         :return:
         """
-        validParam = True
-        if not stock or not strike:
-            validParam = False
-            res = "Option: Displays stock option information based on ticker, strike, type(call or put), " \
-                  "and expiration.\n" + \
-                  "Ex:.option[stock], [strike]\n" + \
-                  "Ex:.option[stock], [strike], [type]\n" + \
-                  "Ex:.option[stock], [strike], [type], [expiration]\n"
-            await ctx.send("```" + res + "```")
-
-        if validParam and s.validateTicker(stock):
+        if stock and s.validateTicker(stock):
             price = s.tickerPrice(stock)
             if price >= 5:
                 res, msg = o.pcOption(stock, strike, type, expir)
@@ -39,14 +31,21 @@ class OptionCommands(commands.Cog):
                     await ctx.send("```" + res + "```")
             else:
                 await ctx.send("```" + stock.upper() + " is not a valid ticker for options.\n" + "```")
-        elif validParam:
+        elif stock:
             await ctx.send("```" + stock.upper() + " is not a valid ticker.\n" + "```")
+        else:
+            res = "Option: Displays stock option information based on ticker, strike, type(call or put), " \
+                  "and expiration.\n" + \
+                  "Ex:.option[stock], [strike]\n" + \
+                  "Ex:.option[stock], [strike], [type]\n" + \
+                  "Ex:.option[stock], [strike], [type], [expiration]\n"
+            await ctx.send("```" + res + "```")
 
     @commands.command(name='f')
     async def findOptionChain(self, ctx, stock=None, type=None, expir=None):
-        """Takes in a stock ticker, an optional expiration date (defaulted to friday expiration [if applicable]),``
-         a type (defaulted to call) and prints the information (Strike, price, volume, OI) on 1 ITM strike and 3 OTM strikes``
-         to discord.
+        """Takes in a stock ticker, an optional expiration date (defaulted to friday expiration [if applicable]),
+        a type (defaulted to call) and prints the information (Strike, price, volume, OI) on 1 ITM strike and 3
+        OTM strikes to discord.
 
         :param ctx:
         :param stock: {1-5} character stock-ticker.
@@ -54,9 +53,16 @@ class OptionCommands(commands.Cog):
         :param expir: Defaulted to 'None'. Represents the expiration date in the format YYYY-MM-DD
         :return:
         """
-        validParam = True
-        if not stock:
-            validParam = False
+        if stock and s.validateTicker(stock):
+            price = s.tickerPrice(stock)
+            if price >= 5:
+                res = o.pcOptionChain(stock, type, expir, price)
+                await ctx.send("```" + res + "```")
+            else:
+                await ctx.send("```" + stock.upper() + " is not a valid ticker for options.\n" + "```")
+        elif stock:
+            await ctx.send("```" + stock.upper() + " is not a valid ticker.\n" + "```")
+        else:
             res = "Option chain: Displays stock option chain information based on ticker, type (call or put), " \
                   "and expiration.\n" + \
                   "Ex: .f [stock]\n" + \
@@ -64,27 +70,15 @@ class OptionCommands(commands.Cog):
                   "Ex: .f [stock], [type], [expiration]\n"
             await ctx.send("```" + res + "```")
 
-        if validParam and s.validateTicker(stock):
-            price = s.tickerPrice(stock)
-            if price >= 5:
-                res = o.pcOptionChain(stock, type, expir, price)
-                await ctx.send("```" + res + "```")
-            else:
-                await ctx.send("```" + stock.upper() + " is not a valid ticker for options.\n" + "```")
-        elif validParam:
-            await ctx.send("```" + stock.upper() + " is not a valid ticker.\n" + "```")
-
     @commands.command(name='read')
-    async def readFridayOptionChain(self, ctx, stock=None):
-        validParam = True
-        if not stock:
-            validParam = False
-            res = "Read Option Info: Displays closest valued options for a ticker with which side is dominating and " \
-                  "top 5 most valued strikes.\n" + \
-                  "Ex. .read [stock]\n"
-            await ctx.send("```" + res + "```")
+    async def readOptionChain(self, ctx, stock=None):
+        """Takes a stock ticker as the parameter to parse multiple option chains on multiple generated expirations
 
-        if validParam and s.validateTicker(stock):
+        :param ctx:
+        :param stock:
+        :return:
+        """
+        if stock and s.validateTicker(stock):
             price = s.tickerPrice(stock)
             if price >= 10:
                 junk = await ctx.send("```" + "Loading the option chain for " + str(stock).upper() + "..." + "```")
@@ -100,8 +94,13 @@ class OptionCommands(commands.Cog):
                     await junk.delete()
             else:
                 await ctx.send("```" + stock.upper() + " is not a valid ticker for options.\n" + "```")
-        elif validParam:
+        elif stock:
             await ctx.send("```" + stock.upper() + " is not a valid ticker.\n" + "```")
+        else:
+            res = "Read Option Info: Displays closest valued options for a ticker with which side is dominating and " \
+                  "top 5 most valued strikes.\n" + \
+                  "Ex. .read [stock]\n"
+            await ctx.send("```" + res + "```")
 
 
 def setup(bot):
